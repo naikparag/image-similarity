36d0fb3e599fbdebc8d0accad7190af8eb1f565f
Bhavin Udavji Suthar <bsuthar@deloitte.com>
2019-07-25 13:32:51 +0530

    Pytorch util added

---
+++ api/ml_pytorch_util.py
@@ -0,0 +1,62 @@
+
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+
+from PIL import Image as PILImage
+
+import config
+import numpy as np
+import ssl, time
+
+
+
+# Load the pretrained model
+model = models.resnet18(pretrained=True)
+# Use the model object to select the desired layer
+layer = model._modules.get('avgpool')
+model.eval()
+scaler = transforms.Scale((224, 224))
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+to_tensor = transforms.ToTensor()
+
+def get_feature_vector(images):
+    time_start = time.time()
+    feature_vector = list(map(get_vector, images))
+    print('pyTorch Feature vector processing! Time elapsed: {} seconds'.format(time.time()-time_start))
+    return feature_vector
+
+def get_vector(image_name):
+    # 1. Load the image with Pillow library
+    img = PILImage.open(config.IMAGE_DIR + image_name)
+    # 2. Create a PyTorch Variable with the transformed image
+    t_img = Variable(normalize(to_tensor(scaler(img))).unsqueeze(0))
+    # 3. Create a vector of zeros that will hold our feature vector
+    #    The 'avgpool' layer has an output size of 512
+    my_embedding = torch.zeros(512)
+    # 4. Define a function that will copy the output of a layer
+    def copy_data(m, i, o):
+        my_embedding.copy_(o.data.squeeze())
+    # 5. Attach that function to our selected layer
+    h = layer.register_forward_hook(copy_data)
+    # 6. Run the model on our transformed image
+    #t_img = t_img.view(1, -1)
+
+    model(t_img)
+    # 7. Detach our copy function from the layer
+    h.remove()
+    # 8. Return the feature vector
+    return my_embedding
+
+def process_cosine_similarity(feature_vector, product):
+    cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+    simi_array = []
+    for vector in feature_vector:
+        cos_sim = cos(product.unsqueeze(0),
+                        vector.unsqueeze(0))
+        simi_array.append(cos_sim)
+
+    return np.array(simi_array)
